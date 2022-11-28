b9f755037ae4d63ccf2aa079b87743e396eb22bb
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-28 08:52:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script >
+  import PageTitle from './components/PageTitle.vue';
+  export default{
+    components:{
+      PageTitle
+    }
+  }
+</script>
+
+<template>
+  <PageTitle/>
+</template>
+
+<style scoped>
+</style>
